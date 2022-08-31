30b666b568c6f812112e8316e6f4fd5bba1a351a
sh <familiehira@outlook.com>
2022-08-31 08:52:03 +0200

    First stage bot responsed

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply('Server info.');
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
 });
 
 // Login to Discord with your client's token
