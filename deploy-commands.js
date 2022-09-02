fdd9956f4481132db133bc40de3ce1dc2828e6b1
sh <familiehira@outlook.com>
2022-09-02 09:33:45 +0200

    commit

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,20 +6,39 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-    new SlashCommandBuilder().setName('vote').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('vote').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
-	
+
+const gameAdd = [{
+	'name': 'game',
+	'description': 'add game',
+	'option': [
+		{
+			'name': 'game_name',
+			'description': 'Name of the game',
+			'type': 3,
+			'required': true
+		}
+	]
+}]
+
 const rest = new REST({ version: '10' }).setToken(token);
 
- 
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: gameAdd, commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,67 +1,25 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits, userMention, messageLink, time } = require('discord.js');
-const { token } = require('./config.json');
-
+const { Client, GatewayIntentBits, version, SlashCommandBuilder} = require('discord.js');
+const { token, clientId, guildId } = require('./config.json');
 const { Routes } = require('discord.js');
 const {REST} = require("@discordjs/rest");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent] });
 
-// When the client is ready, run this code (only once)
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
 
-const rest = new REST({ version: '10' }).setToken(token);
+    const { commandName } = interaction;
 
-async () => {
-    // client.on('interactionCreate', (interaction) => {
-    //     if(interaction.isChatInputCommand()){
-    //         console.log(interaction.options)
-    //         interaction.reply({content: 'Added'})
-    //     }})
+    if(interaction.isChatInputCommand()){
+        console.log(interaction.options)
+        interaction.reply({content: 'Added'})
 
-    // const command = [{
-    //     'name': 'game',
-    //     'description': 'add game',
-    //     'options': [
-    //         {
-    //             'name': 'game name',
-    //             'description': 'Name of your favourite game',
-    //             'type': 3,
-    //             'required': true
-    //         }
-    //     ]
-    // }]
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply('Server info.');
-    } else if (commandName === 'user') {
-        await interaction.reply('User info.');
-    } else if (commandName === 'vote') {
-        const message = await interaction.reply({ content: "Give me your 3 favourite games then everyone can vote. Give them like this: 'Halo, fortnite, fall guys, amongus'", fetchReply: true});
-		message.react('😄');
-        message.awaitReactions({time:6000, errors:['time']})
-        .then(collected => {
-            const reaction = collected.first();
-            if(reaction.emoji.name === '😄'){
-                message.reply('You reacted with a smile!');
-            }
-        });
-
-        try {
-            const data = await rest.put(
-                Routes.applicationGuildCommands(clientId, guildId),
-                { body: command },
-            );
-
-            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-        } catch (error) {
-            console.error(error);
-
-        }
-    }
-};
+}});
 
 // Login to Discord with your client's token
 client.login(token);
