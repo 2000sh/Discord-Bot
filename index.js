8dfff24bbe8fb2d915d310286a692a4720e79cb1
sh <familiehira@outlook.com>
2022-09-02 10:34:16 +0200

    removing deploy commands all in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits, version, SlashCommandBuilder} = require('discord.js');
-const { token, clientId, guildId } = require('./config.json');
-const { Routes } = require('discord.js');
-const {REST} = require("@discordjs/rest");
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('./config.json');
+const { SlashCommandBuilder, Routes, Client, GatewayIntentBits} = require('discord.js');
 
-// Create a new client instance
+
+const rest = new REST({ version: '10' }).setToken(token);
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -14,21 +14,44 @@ const client = new Client({
     ],
 });
 
-client.on('messageCreate', (message) => {
-
-    console.log(message.reply(message.content))
-})
-
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate',  (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction;
+    if(interaction.commandName === 'game'){
+        interaction.reply({content: 'Listed'})
+    }
 
-    if(interaction.isChatInputCommand()){
-        console.log(interaction.options)
-        interaction.reply({content: 'Added'})
+});
 
-}});
+async function main() {
+    const commands = [
+        new SlashCommandBuilder().setName('game').setDescription('Adds a game to the game'),
+    ]
+        .map(command => command.toJSON());
 
-// Login to Discord with your client's token
-client.login(token);
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: commands,
+        });
+        client.login(token);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
