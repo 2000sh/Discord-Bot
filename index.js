a80f126e2fa95f3ef16c773c32592d55c17600d3
sh <familiehira@outlook.com>
2022-09-02 11:19:11 +0200

    console log added game

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { REST } = require('@discordjs/rest');
 const { clientId, guildId, token } = require('./config.json');
 const { SlashCommandBuilder, Routes, Client, GatewayIntentBits} = require('discord.js');
 
-
 const rest = new REST({ version: '10' }).setToken(token);
 
 const client = new Client({
@@ -17,17 +16,37 @@ const client = new Client({
 client.on('interactionCreate',  (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if(interaction.commandName === 'game'){
-        interaction.reply({content: 'Listed'})
+    if(interaction.isChatInputCommand()) {
+        const data = 'abc'
+        if(interaction.commandName === 'game') {
+            let user = interaction.user.username + '#' + interaction.user.discriminator
+            console.log(interaction.options)
+            console.log(user)
+            interaction.reply({content: 'listed'})
+        }
     }
 
 });
 
 async function main() {
+    //Opportunity to add a game
     const commands = [
-        new SlashCommandBuilder().setName('game').setDescription('Adds a game to the game'),
+        {
+            name: 'game',
+            description: 'add game',
+            options: [
+                {
+                    name: 'add',
+                    description: 'which game you try to add',
+                    type: '3'
+                }
+            ]
+        },
+        {
+            name: 'games',
+            description: 'data logs'
+        }
     ]
-        .map(command => command.toJSON());
 
     try {
         console.log('Started refreshing application (/) commands.');
